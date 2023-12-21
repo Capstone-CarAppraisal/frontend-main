e955cdb15b7efc64599ce0d23680bdcea0675fd1
peemknn <kunanontpeem333@gmail.com>
2023-12-21 13:16:21 +0700

    Add pricecard

--- src/app/(withnav)/estimate/page.tsx
+++ src/app/(withnav)/estimate/page.tsx
@@ -1,40 +1,31 @@
+import PriceCard from "@/components/Estimate/PriceCard";
 import SideBar from "@/components/Sidebar/Sidebar";
 
 export default function EstimatePage() {
   return (
     <main>
       <div>
-        <div className="flex flex-col bg-white border shadow-sm rounded-xl m-10 p-4 md:p-5 dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-          <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-            Card title
+        <div className="flex flex-col bg-white m-4 border shadow-sm rounded-xl m-10 p-6 h-fit">
+          <h3 className="text-2xl sm:text-4xl font-sans font-bold text-dark-blue">
+            ผลการประเมิน
           </h3>
-          <p className="mt-1 text-xs font-medium uppercase text-gray-500 dark:text-gray-500">
-            Card subtitle
-          </p>
-          <p className="mt-2 text-gray-500 dark:text-gray-400">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a
-            className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-            href="#"
+          <div
+            className="max-h-[calc(90vh-150px)] 
+            overflow-y-auto
+            [&::-webkit-scrollbar]:w-2
+            [&::-webkit-scrollbar-track]:bg-gray-100
+            [&::-webkit-scrollbar-thumb]:bg-gray-300
+           sm:grid sm:grid-cols-2 gap-2 mt-8"
           >
-            Card link
-            <svg
-              className="flex-shrink-0 w-4 h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width={24}
-              height={24}
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m9 18 6-6-6-6" />
-            </svg>
-          </a>
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+            <PriceCard />
+          </div>
         </div>
       </div>
     </main>
---
+++ src/components/Estimate/PriceCard.tsx
@@ -0,0 +1,9 @@
+export default function PriceCard() {
+  return (
+    <div className="rounded-xl bg-light-blue  h-[250px] w-full items-center">
+      <div className="flex flex-col">
+        <h1 className="text-5xl font-bold font-sans text-dark-blue ">฿448k</h1>
+      </div>
+    </div>
+  );
+}
