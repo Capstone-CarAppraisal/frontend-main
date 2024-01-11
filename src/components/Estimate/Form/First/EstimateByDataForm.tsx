70f421e1926d1cc203aafe7fb93d66fa94bc0d98
peemknn <kunanontpeem333@gmail.com>
2024-01-11 09:41:22 +0700

    Add car interior form

--- src/components/Estimate/Form/First/EstimateByDataForm.tsx
+++ src/components/Estimate/Form/First/EstimateByDataForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import SelectInputWithLabel from "@/components/Shared/SelectInputWithLabel";
 import React, { useState } from "react";
 
 export default function EstimateByDataForm({
@@ -31,75 +32,22 @@ export default function EstimateByDataForm({
               <div className="grid grid-row-2 w-[80%] mb-20">
                 <div className="grid grid-cols-2 mb-10">
                   <div className="px-4">
-                    <label
-                      htmlFor="hs-select-label"
-                      className="block text-sm mb-2 text-black text-xl font-normal"
-                    >
-                      ยี่ห้อ
-                    </label>
-                    <select
-                      id="hs-select-label"
-                      className="py-3 px-4 pe-9 block  border-[#BCBCBC] w-full rounded-lg text-sm focus:border-blue-500"
-                    >
-                      <option>เลือกยี่ห้อรถ</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                    </select>
+                    <SelectInputWithLabel label="ยี่ห้อ" name="เลือกยี่ห้อรถ" />
                   </div>
                   <div className="px-4">
-                    <label
-                      htmlFor="hs-select-label"
-                      className="block text-sm mb-2 text-black text-xl font-normal"
-                    >
-                      รุ่น
-                    </label>
-                    <select
-                      id="hs-select-label"
-                      className="py-3 px-4 pe-9 block w-full border-[#BCBCBC] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 "
-                    >
-                      <option>เลือกรุ่นรถ</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                    </select>
+                    <SelectInputWithLabel label="รุ่น" name="เลือกรุ่นรถ" />
                   </div>
                 </div>
 
                 <div className="grid grid-cols-2">
                   <div className="px-4">
-                    <label
-                      htmlFor="hs-select-label"
-                      className="block text-sm mb-2 text-black text-xl font-normal"
-                    >
-                      ประเภทรถ
-                    </label>
-                    <select
-                      id="hs-select-label"
-                      className="py-3 px-4 pe-9 block  border-[#BCBCBC] w-full rounded-lg text-sm focus:border-blue-500"
-                    >
-                      <option>เลือกประเภทรถ</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                    </select>
+                    <SelectInputWithLabel
+                      label="ประเภทรถ"
+                      name="เลือกประเภทรถ"
+                    />
                   </div>
                   <div className="px-4">
-                    <label
-                      htmlFor="hs-select-label"
-                      className="block text-sm mb-2 text-black text-xl font-normal"
-                    >
-                      สี
-                    </label>
-                    <select
-                      id="hs-select-label"
-                      className="py-3 px-4 pe-9 block w-full border-[#BCBCBC] rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 "
-                    >
-                      <option>เลือกสีรถ</option>
-                      <option>1</option>
-                      <option>2</option>
-                      <option>3</option>
-                    </select>
+                    <SelectInputWithLabel label="สี" name="เลือกสีรถ" />
                   </div>
                 </div>
               </div>
@@ -124,12 +72,97 @@ export default function EstimateByDataForm({
         </div>
       )}
       {step === 2 && (
-        <div className="flex justify-center w-full h-[60vh] font-sans pt-8">
+        <div className="flex justify-center w-full h-[70vh] font-sans pt-8">
           <div className="flex flex-col w-[80vw]">
             <div className="bg-dark-blue font-bold text-white text-2xl w-fit px-5 py-3">
               กรอกลักษณะภายในรถ
             </div>
             <div className="relative border border-2 border-[#D9D9D9] h-full flex justify-center items-center">
+              <div className="grid grid-row-3 w-[87%] mb-20 gap-3">
+                <div className="grid grid-cols-4 gap-20">
+                  <div>
+                    <SelectInputWithLabel label="เลขซีซี" name="เลือกเลขซีซี" />
+                  </div>
+                  <div className="col-span-3">
+                    <SelectInputWithLabel
+                      label="รุ่นย่อย"
+                      name="เลือกรุ่นย่อยรถ"
+                    />
+                  </div>
+                </div>
+                <div className="grid grid-cols-4 gap-20">
+                  <div>
+                    <SelectInputWithLabel label="ปีรถ" name="เลือกปีรถ" />
+                  </div>
+
+                  <div className="col-span-3">
+                    <label
+                      htmlFor="hs-select-label"
+                      className="block text-sm mb-2 text-black text-xl font-normal"
+                    >
+                      ระบบเกียร์
+                    </label>
+
+                    <div className="flex gap-x-32 mt-5">
+                      <div className="flex items-center">
+                        <input
+                          type="radio"
+                          name="hs-radio-group"
+                          className="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                          id="hs-radio-group-1"
+                        />
+                        <label
+                          htmlFor="hs-radio-group-1"
+                          className="text-xl text-gray-500 ms-2 dark:text-gray-400"
+                        >
+                          เกียร์ธรรมดา
+                        </label>
+                      </div>
+                      <div className="flex items-center">
+                        <input
+                          type="radio"
+                          name="hs-radio-group"
+                          className="shrink-0 mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                          id="hs-radio-group-3"
+                        />
+                        <label
+                          htmlFor="hs-radio-group-3"
+                          className="text-xl text-gray-500 ms-2 dark:text-gray-400"
+                        >
+                          เกียร์อัตโนมัติ
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="grid grid-cols-4 gap-20">
+                  <div>
+                    <SelectInputWithLabel
+                      label="โฉมปีเริ่มต้น"
+                      name="เลือกโฉมปี"
+                    />
+                  </div>
+                  <div>
+                    <SelectInputWithLabel
+                      label="โฉมปีล่าสุด"
+                      name="เลือกโฉมปี"
+                    />
+                  </div>
+                  <div className="col-span-2">
+                    <label
+                      htmlFor="hs-select-label"
+                      className="block text-sm mb-2 text-black text-xl font-normal"
+                    >
+                      เลขไมล์ (km)
+                    </label>
+                    <input
+                      type="text"
+                      className="py-3 px-4 block w-full border-[#BCBCBC] text-black placeholder:text-[#BCBCBC] rounded-lg text-lg focus:border-blue-500 focus:ring-blue-500"
+                      placeholder="ระบุเลขไมล์"
+                    />
+                  </div>
+                </div>
+              </div>
               <div className="absolute bottom-5  space-x-10">
                 <button
                   type="button"
