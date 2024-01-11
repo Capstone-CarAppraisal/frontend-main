65ab0a20836a68db2e2ac852fa945060dc4bafbd
peemknn <kunanontpeem333@gmail.com>
2024-01-11 14:40:52 +0700

    change estimate layout

--- src/app/(withnav)/estimate/layout.tsx
+++ src/app/(withnav)/estimate/layout.tsx
@@ -1,13 +1,14 @@
 import SideBar from "@/components/Sidebar/Sidebar";
 import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Navbar/Footer";
 export default function EstimatePageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-white h-[80vh]">
-      <div>{children}</div>
+    <div className="bg-white">
+      <div className="mb-[50px]">{children}</div>
     </div>
   );
 }
