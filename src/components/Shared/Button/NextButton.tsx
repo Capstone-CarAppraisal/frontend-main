2d434d59e0b4a162c4931eea64fea78f140fed94
Peemknn <kunanontpeem333@gmail.com>
2024-02-18 00:39:44 +0700

    add disabled

--- src/components/Shared/Button/NextButton.tsx
+++ src/components/Shared/Button/NextButton.tsx
@@ -4,13 +4,16 @@ import NextArrow from "../../../../public/assets/Nav/NextArrow.svg";
 
 export default function NextButton({
   handleClick,
+  isDisabled,
 }: {
   handleClick: MouseEventHandler;
+  isDisabled?: boolean;
 }) {
   return (
     <button
       type="button"
       onClick={handleClick}
+      disabled={!isDisabled}
       className="py-2 px-3 w-[13vw] flex items-center justify-center  text-lg font-semibold rounded-3xl border border-transparent bg-rose text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
     >
       <span className="flex items-center gap-x-5">
