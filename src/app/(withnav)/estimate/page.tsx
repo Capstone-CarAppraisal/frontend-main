4f80133dd7f261bed1f267547b2246af015ee0f8
Peemknn <kunanontpeem333@gmail.com>
2024-02-20 22:20:24 +0700

    Add toaster

--- src/app/(withnav)/estimate/page.tsx
+++ src/app/(withnav)/estimate/page.tsx
@@ -1,10 +1,12 @@
 import Form from "@/components/Estimate/Form/Form";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function EstimatePage() {
   return (
     <main>
       <>
         <div className="mt-[150px]">
+          <Toaster />
           <Form />
         </div>
       </>
