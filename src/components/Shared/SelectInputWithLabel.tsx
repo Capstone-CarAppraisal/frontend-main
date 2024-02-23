9d5f3486399c99c923978de43e68e0b375d63491
Peemknn <kunanontpeem333@gmail.com>
2024-02-23 21:36:04 +0700

    fix label color

--- src/components/Shared/SelectInputWithLabel.tsx
+++ src/components/Shared/SelectInputWithLabel.tsx
@@ -15,14 +15,10 @@ export default function SelectInputWithLabel({
 }) {
   return (
     <div className="">
-      <label
-        htmlFor="hs-select-label"
-        className="block text-sm mb-2 text-black text-xl font-normal"
-      >
+      <label className="block text-sm mb-2 text-dark-blue text-xl font-normal">
         {label}
       </label>
       <select
-        id="hs-select-label"
         className={`py-3 px-4 pe-9 block  border-[#BCBCBC] ${
           isSelected ? "text-black" : "text-[#BCBCBC]"
         } w-full rounded-lg  text-lg focus:border-blue-500 focus:text-black`}
