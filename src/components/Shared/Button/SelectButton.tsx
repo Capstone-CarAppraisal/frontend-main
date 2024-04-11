04415f8359f4d7dc46029e837d2e44c5253354f0
Peemknn <kunanontpeem333@gmail.com>
2024-04-11 17:23:06 +0700

    add select summary viewmode button

---
+++ src/components/Shared/Button/SelectButton.tsx
@@ -0,0 +1,30 @@
+export default function SelectButton({
+  onChange,
+  activeMode,
+}: {
+  onChange: any;
+  activeMode: any;
+}) {
+  return (
+    <div className="flex rounded-lg shadow-sm">
+      <button
+        type="button"
+        className={`w-[160px] py-3 px-4  items-center gap-x-2 text-2xl font-bold text-white shadow-sm ${
+          activeMode === 0 ? "bg-dark-blue" : "bg-lightgrey"
+        }`}
+        onClick={() => onChange(0)}
+      >
+        ทั่วไป
+      </button>
+      <button
+        type="button"
+        className={`w-[160px] py-3 px-4  items-center gap-x-2 text-2xl font-bold text-white  shadow-sm ${
+          activeMode === 1 ? "bg-dark-blue" : "bg-lightgrey"
+        }`}
+        onClick={() => onChange(1)}
+      >
+        เชี่ยวชาญ
+      </button>
+    </div>
+  );
+}
