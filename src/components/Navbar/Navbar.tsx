72e65698415f12ea11dff20afb31a7dd5a815bf4
peemknn <kunanontpeem333@gmail.com>
2024-05-24 16:37:33 +0700

    Remove mobile responsive from navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,10 @@
 "use client";
-import { useState } from "react";
-import Hamburger from "./Hamburger";
 import Link from "next/link";
 import Image from "next/image";
 import questionIcon from "@/../public/assets/Nav/questionIcon.svg";
 import homeLogo from "@/../public/assets/Nav/homeLogo.svg";
 import closeIcon from "@/../public/assets/Nav/closeIcon.svg";
-
 export default function Navbar() {
-  const [isNavOpen, setNavIsOpen] = useState(false);
-
-  const handleToggleNavMenu = () => {
-    setNavIsOpen((prev) => !prev);
-  };
-
   return (
     <div className="z-50 fixed top-0 right-0 left-0">
       <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-dark-blue text-sm py-4">
@@ -36,50 +27,24 @@ export default function Navbar() {
                       alt="home-logo"
                       width={50}
                       height={50}
-                    />
+                    ></Image>
                   </div>
                 </Link>
               </div>
             </div>
-
-            <div className="sm:hidden">
-              <button
-                type="button"
-                className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2"
-                aria-controls="navbar-collapse-with-animation"
-                aria-label="Toggle navigation"
-                onClick={handleToggleNavMenu}
-              >
-                {isNavOpen ? (
-                  <Image
-                    src={closeIcon}
-                    alt="close-icon"
-                    width={24}
-                    height={24}
-                  />
-                ) : (
-                  <Hamburger />
-                )}
-              </button>
-            </div>
           </div>
           <div
             id="navbar-collapse-with-animation"
-            className={`hs-collapse ${
-              isNavOpen ? "block" : "hidden"
-            } overflow-hidden transition-all duration-300 basis-full grow sm:block`}
+            className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block "
           >
             <div className="font-sans font-semibold flex flex-col align-center justify-center h-screen sm:h-[65px] text-xl flex-col items-center gap-y-20 gap-x-8 text-white mt-5 sm:flex-row sm:items-center sm:space-x-3 sm:justify-end sm:mt-0 sm:ps-5 sm:mr-8">
-              <Link href="/about-us">เกี่ยวกับเรา</Link>
+              <Link className="" href="/about-us">
+                เกี่ยวกับเรา
+              </Link>
               <Link className="flex flex-row items-center" href="#">
                 วิธีใช้งาน
                 <span className="pl-2">
-                  <Image
-                    src={questionIcon}
-                    alt="question-icon"
-                    width={24}
-                    height={24}
-                  />
+                  <Image src={questionIcon} alt="question-icon"></Image>
                 </span>
               </Link>
               <div className="sm:hidden">
